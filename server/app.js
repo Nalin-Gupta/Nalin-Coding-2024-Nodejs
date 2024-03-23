c32bf3978945fed45d6a16d06ddb19f1551d4d93
nalin <nalin.kamesh@jungleworks.com>
2024-03-23 12:20:01 +0530

    Add : MongoDB Integration + Profile Schema

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 const app = express();
 const port =  process.env.PORT || 3000;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+//Mongo Setup 
+let mongoServer;
+mongoServer = new MongoMemoryServer();
+mongoServer.getUri().then((mongoUri) => {
+  mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
 
 // routes
 app.use('/', require('./routes/profile')());
---
+++ server/models/Profile.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    mbti: {
+        type: String,
+        enum: ['ISFJ', 'ISTJ'],
+        required: true,
+    },
+    enneagram: {
+        type: String,
+        enum: ['9w3', '1w2'],
+        required: true,
+    },
+    variant: {
+        type: String,
+        enum: ['sp/so', 'sp/sx'],
+        required: true,
+    },
+    tritype: {
+        type: Number,
+        required: true,
+    },
+    socionics: {
+        type: String,
+        required: true,
+    },
+    sloan: {
+        type: String,
+        enum: ['RCOEN', 'RLOIN'],
+        required: true,
+    },
+    psyche: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+});
+
+const Profile = mongoose.model('Profile', profileSchema);
+
+module.exports = Profile;
